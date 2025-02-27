50e7c210c5d1296127faaee5da6ee5b1a03b8745
Cristianv9 <156293750+Cristianv9@users.noreply.github.com>
2025-02-27 17:57:05 +0000

    Transfer everything from codepen

--- script.js
+++ script.js
@@ -1,2 +1,51 @@
 // ✅ gsap loaded? 
 console.log( gsap );
+
+gsap.from("#bug", {
+    y: -20, 
+    opacity: 0, 
+    duration: 1, 
+    ease: "power2.out"
+});
+
+gsap.to("#bug", {
+    x: -window.innerWidth - 100, 
+    duration: 8, 
+    ease: "linear"
+});
+
+gsap.to("#bug", {
+    y: "+=2", 
+    duration: 0.3, 
+    repeat: -1, 
+    yoyo: true, 
+    ease: "power1.inOut"
+});
+
+gsap.to(".legs", {
+    y: "+=2", 
+    duration: 0.2, 
+    repeat: -1, 
+    yoyo: true, 
+    ease: "power1.inOut",
+    stagger: 0.1 
+});
+
+
+// Animate Webhook Icon
+gsap.to("#webhookIcon", {
+    rotation: 360, 
+    duration: .25,  
+    repeat: -1,   
+    ease: "linear" 
+});
+
+gsap.to("#rocket", {
+    y: -500,
+    x: 500,
+    scale: 1.5,     
+    opacity: 0,     
+    duration: 2,   
+    ease: "power2.in", 
+    delay: 1 // 
+});
